07b7cd0d651e37ac2328795705da3bc659fe98cd
scohenjanes5 <scohenjanes@brandeis.edu>
2022-06-02 10:45:12 -0400

    Can read polymer from existing files
    Execution times are about a quarter second compared to multiple seconds when generating a larger polymer every run.

--- LogP/MakePolymer.py
+++ LogP/MakePolymer.py
@@ -1,9 +1,8 @@
 from rdkit import Chem
 from rdkit.Chem import AllChem,Draw,Descriptors,rdFreeSASA
-import argparse
+import argparse,os
 
 ###Built-in dict. Can be divorced from this file later
-
 monomer_dict={
 
     #monomers for polyamides
@@ -59,8 +58,8 @@ def getArgs():
     parser.add_argument("-v", "--verbose", default=False, action="store_true", help="Set increased verbosity. Will draw polymer to polymer.png unless alternate name set by -d option.")
     parser.add_argument("-c","--calculation", type=str, nargs='*', help="Type of calculation(s) to be performed input as a space-separated list. Options are LogP, SA (surface area) and RG (radius of gyration).")
     parser.add_argument("-f","--file", type=str, help="The name/path of the file you wish to save the mol to. Supported formats are .pdb, .xyz and .mol")
+    parser.add_argument("-r", "--read", type=str, help="The name/path to file you wish to import. Supported formats are .pdb and .mol")
     args=parser.parse_args()
-
     return args
 
 def createPolymerSMILES(i,n,m,t):
@@ -79,9 +78,8 @@ def createPolymerSMILES(i,n,m,t):
                 repeat=1 #reset coef.
     else:
         repeat_unit=monomer_dict[m] #if not a list, look for the corresponding smiles in the dictionary, will throw error if not included.
-
+    
     polymer_SMILES=smiles_inators[0]+n*repeat_unit+smiles_inators[1] #concatonate all parts as a big SMILES.
-
     return polymer_SMILES
 
 def bestConformer(pol_h,numConfs,seed): #currently unused. See notes or question in optPol().
@@ -118,21 +116,32 @@ def optPol(smiles):
 def drawPol(pol,drawName):
     Chem.Draw.MolToFile(pol,drawName)
 
-def write_or_read_pol(pol_h,name): 
-    #in the future it may be useful to read a specified file if it doesn't yet exist.
-    #that would mean we can skip the time-consuming optimization process and go right to calculations.
+def write_or_read_pol(pol_h_or_str,name):
     ext=name.split(".")[1]
-    status=0
-    if ext=="xyz":
-        Chem.MolToXYZFile(pol_h,name)
-    elif ext == "pdb":
-        Chem.MolToPDBFile(pol_h,name)
-    elif ext == "mol":
-        Chem.MolToMolFile(pol_h,name)
+    if type(pol_h_or_str)==str: #"Read" if in read mode.
+        if os.path.exists(name):
+            if ext == "pdb":
+                pol_h = Chem.MolFromPDBFile(name)
+            elif ext == "mol":
+                pol_h = Chem.MolFromMolFile(name)
+            else:
+                print("unsuported extention:",ext,"Please use .pdb, or .mol")
+                quit()
+            return pol_h
+        else:
+            raise FileNotFoundError(name)
     else:
-        print("unsuported extention:",ext,"Please use .pdb, .xyz or .mol")
-        status=1
-    return status
+        exit_status=0
+        if ext == "xyz":
+            Chem.MolToXYZFile(pol_h_or_str,name)
+        elif ext == "pdb":
+            Chem.MolToPDBFile(pol_h_or_str,name)
+        elif ext == "mol":
+            Chem.MolToMolFile(pol_h_or_str,name)
+        else:
+            print("unsuported extention:",ext,"Please use .pdb, .xyz or .mol")
+            exit_status=1
+        return exit_status
 
 def Sasa(pol_h):#,best_conf_id):
     # Now calculate LogP and SASA
@@ -143,8 +152,7 @@ def Sasa(pol_h):#,best_conf_id):
     
     radii = Chem.rdFreeSASA.classifyAtoms(pol_h)
     #sasa = Chem.rdFreeSASA.CalcSASA(pol_h, radii, confIdx=best_conf_id)
-    sasa = Chem.rdFreeSASA.CalcSASA(pol_h, radii)
-    
+    sasa = Chem.rdFreeSASA.CalcSASA(pol_h, radii)    
     return sasa
 
 def LogP(pol_h):
@@ -180,38 +188,48 @@ def doCalcs(pol_h,calcs):
 
 def main():
     args=getArgs()
-    #Two cases: one monomer or supermonomer.
-    #If both are specified something is wrong.
-    if args.single_monomer is not None and args.super_monomer is not None:
-        raise argparse.ArgumentError("Cannot specify both single and super monomers")
-    
-    #This gives a list of components of a super-monomer or just the string used for single monomer in dict
-    repeat_unit=list(filter(None,[args.single_monomer,args.super_monomer]))[0]
+    if args.read is None: #then get polymer parameters from CLI arguments.
+        #Two cases: one monomer or supermonomer.
+        #If both are specified something is wrong.
+        if args.single_monomer is not None and args.super_monomer is not None:
+            raise argparse.ArgumentError("Cannot specify both single and super monomers")
+        
+        #This gives a list of components of a super-monomer or just the string used for single monomer in dict
+        repeat_unit=list(filter(None,[args.single_monomer,args.super_monomer]))[0]
 
-    polSMILES=createPolymerSMILES(args.initiator,args.n,repeat_unit,args.terminator)
+        polSMILES=createPolymerSMILES(args.initiator,args.n,repeat_unit,args.terminator)
 
-    if args.verbose: #extra info if requested
-        print("Polymer interpreted as:",args.initiator,str(args.n),"*",str(repeat_unit),args.terminator)
-        print("This gives the following SMILES:",polSMILES)
+        if args.verbose: #extra info if requested
+            print("Polymer interpreted as:",args.initiator,str(args.n),"*",str(repeat_unit),args.terminator)
+            print("This gives the following SMILES:",polSMILES)
+
+        pol_h,pol=optPol(polSMILES) #both are mol objects
+    else: #get mol from file
+        pol_h=write_or_read_pol("Read",args.read)
+
+    if args.verbose: #more extra information
         print("requested calculations are",args.calculation)
 
-    pol_h,pol=optPol(polSMILES) #both are mol objects
-
-    if args.file is not None:
+    #saving the polymer to a file.
+    if args.file is not None: #technically nothing wrong with using this as a roundabout way of converting between filetypes
         if args.verbose:
             print("attempting to save molecule to",args.file)
         stat=write_or_read_pol(pol_h,args.file)
         if args.verbose and stat == 0:
             print("success.")
 
-    if args.draw is not None:
+    #drawing an picture of the polymer.
+    #drawings with optimized geoms are ugly in 2D. Fix so it "flattens out" so we can get drawing of unknown file
+    if args.draw is not None and args.read is None:
         drawName=args.draw.split(".")[0]+".png"
         drawPol(pol,drawName)
     else:
-        if args.verbose:
+        if args.verbose and args.read is None:
+            #if we can flatten out the mol change this trigger too.
             #produce image if increased verbocity is requested even if no name is set.
             drawPol(pol,"polymer.png")
 
+    #doing only the specified calculations.
     if args.calculation is not None:
         data=doCalcs(pol_h,args.calculation)
         print(data)
