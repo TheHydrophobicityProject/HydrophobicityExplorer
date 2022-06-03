adbb41cd6e2d4a2448cbb4bd40df3eeb5fce42cd
scohenjanes5 <scohenjanes@brandeis.edu>
2022-06-03 12:20:52 -0400

    add amino acids to monomer dict
    also remove now obsolete notebook.

--- LogP/MakePolymer.py
+++ LogP/MakePolymer.py
@@ -5,6 +5,40 @@ import argparse,os,csv
 ###Built-in dict. Can be divorced from this file later
 monomer_dict={
 
+    #amino acids
+    'ala': 'C[C@@H](C(=O)O)N',
+    'ala-prot': 'C[C@@H](C(=O)[O-])[NH3+]',
+    'beta-ala': 'NCCC(O)=O',
+    'beta-ala-prot': '[NH3+]CCC([O-])=O',
+    'L-cystiene': 'C([C@@H](C(=O)O)N)S',
+    'L-cystiene-prot': 'C([C@@H](C(=O)[O-])[NH3+])S',
+    'L-DOPA': 'N[C@H](C(O)=O)CC1=CC=C(O)C(O)=C1',
+    'L-DOPA-prot': '[NH3+][C@H](C([O-])=O)CC1=CC=C(O)C(O)=C1',
+    'glycine': 'C(C(=O)O)N',
+    'glycine-prot': 'C(C(=O)[O-])[NH3+]',
+    'L-isoleucine': 'N[C@@H](C(C)CC)C(O)=O',
+    'L-isoleucine-prot': '[NH3+][C@@H](C(C)CC)C([O-])=O',
+    'L-leucine': 'N[C@@H](CC(C)C)C(O)=O',
+    'L-leucine-prot': '[NH3+][C@@H](CC(C)C)C([O-])=O',
+    'L-phenylala': 'N[C@@H](CC1=CC=CC=C1)C(O)=O',
+    'L-phenylala-prot': '[NH3+][C@@H](CC1=CC=CC=C1)C([O-])=O',
+    'L-phenylala_F': 'N[C@@H](CC1=CC=C(F)C=C1)C(O)=O',
+    'L-phenylala_F-prot': '[NH3+][C@@H](CC1=CC=C(F)C=C1)C([O-])=O',
+    'L-phenylala_Cl': 'N[C@@H](CC1=CC=C(Cl)C=C1)C(O)=O',
+    'L-phenylala_Cl-prot': '[NH3+][C@@H](CC1=CC=C(Cl)C=C1)C([O-])=O',
+    'L-phenylala_N': 'N[C@@H](CC1=CC=C(N)C=C1)C(O)=O',
+    'L-phenylala_N-prot': '[NH3+][C@@H](CC1=CC=C(N)C=C1)C([O-])=O',
+    'L-phenylala_CN': 'N[C@@H](CC1=CC=C(C#N)C=C1)C(O)=O',
+    'L-phenylala_CN-prot': '[NH3+][C@@H](CC1=CC=C(C#N)C=C1)C([O-])=O',
+    'L-phenylgly': 'N[C@@H](C1=CC=CC=C1)C(O)=O',
+    'L-phenylgly-prot': '[NH3+][C@@H](C1=CC=CC=C1)C([O-])=O',
+    'L-proline': 'C1C[C@H](NC1)C(=O)O',
+    'L-proline-prot': '[O-]C(=O)[C@H](CCC2)[NH2+]2',
+    'L-threonine': 'C[C@H]([C@@H](C(=O)O)N)O',
+    'L-threonine-prot': 'C[C@H]([C@@H](C(=O)[O-])[NH3+])O',
+    'L-valine': 'CC(C)[C@@H](C(=O)O)N',
+    'L-valine-prot': 'CC(C)[C@@H](C(=O)[O-])[NH3+]',
+
     #monomers for polyamides
     'Nylon6': 'CCCCCC(=O)N',
 
@@ -37,13 +71,14 @@ monomer_dict={
 }
 
 #initiator dictionary (i.e. endgroup of polymer chain)
-init_dict={'Benzyl': 'c1ccccc1CO', 
-           'Butyl': 'CCCC',
-           'Hydroxyl': 'O', 
-           'Hydrogen': '',
-           'Methoxy': 'CO', 
-           'Methyl': 'C', 
-           'Vinyl': 'C=C'
+init_dict={
+    'Benzyl': 'c1ccccc1CO', 
+    'Butyl': 'CCCC',
+    'Hydroxyl': 'O', 
+    'Hydrogen': '',
+    'Methoxy': 'CO', 
+    'Methyl': 'C', 
+    'Vinyl': 'C=C'
 }
 
 def getArgs():
