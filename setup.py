259fa25855a2130c8d2bab9ab850f7b38405f7bb
scohenjanes5 <scohenjanes@brandeis.edu>
2022-07-16 11:49:17 -0400

    add script to drag race optimization methods

---
+++ setup.py
@@ -0,0 +1,11 @@
+# from setuptools import setup
+
+# setup(name='mhp',
+#       version='0.1',
+#       description='Polymer Hydrophobicity Toolbox',
+#       url='https://github.com/scohenjanes5/MHP',
+#       author='Sander Cohen-Janes',
+#       author_email='sochenjanes@brandeis.edu',
+#       license='MIT',
+#       packages=['mhp'],
+#       zip_safe=False)
--- testing/test_pol.py
+++
@@ -1,6 +0,0 @@
-import pytest
-from ..LogP.MakePolymer import main
-
-def test_pol():
-    assert main() == 0
-#doesn't really work now.
