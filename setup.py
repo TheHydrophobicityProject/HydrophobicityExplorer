c47743c05a7f96ea243def02483d28182a6ea917
scohenjanes5 <scohenjanes@brandeis.edu>
2022-08-31 21:47:48 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,15 +9,15 @@ setuptools.setup(
     description="Facilitates solubility calculations on a wide range of polymers.",
     long_description=open('README.md').read(),
     packages=setuptools.find_packages(),
-    install_requires=["rdkit", "argparse", "scipy", "csv"],
+    install_requires=["rdkit", "argparse", "scipy", "pandas"],
     classifiers=[
         'Programming Language :: Python',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.10',
     ],
     entry_points={
-        'console_scripts': [
-            # command = package.module:function
+        'console_scripts': [ #this section allows us to use functions from the command line. Arguments are ok because main() uses argparse
+            # 'command = package.module:function',
             'makePol = mhp.MakePolymer:main',
             'customPol = mhp.custom_input_to_mol_file:main',
             'randomPol = mhp.random_polymer_to_mol_file:main',
