40c5c2c4dc3f3a9a699abe3c3929836a6ce359e6
scohenjanes5 <scohenjanes@brandeis.edu>
2022-09-21 09:00:18 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setuptools.setup(
     description="Facilitates solubility calculations on a wide range of polymers.",
     long_description=open('README.md').read(),
     packages=setuptools.find_packages(),
-    install_requires=["rdkit", "argparse", "scipy", "pandas"],
+    install_requires=["rdkit", "scipy", "pandas", "matplotlib"],
     classifiers=[
         'Programming Language :: Python',
         'Programming Language :: Python :: 3',
