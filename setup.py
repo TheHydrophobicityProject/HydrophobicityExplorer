e41d5dc928d0b2e52833e50640115c52521d6984
scohenjanes5 <scohenjanes@brandeis.edu>
2023-07-17 16:04:02 -0400

    Update author info

--- setup.py
+++ setup.py
@@ -3,9 +3,9 @@ import setuptools
 setuptools.setup(
     name="mhp",
     version="0.1.3.4",
-    url="https://github.com/scohenjanes5/MHP",
+    url="https://github.com/TheHydrophobicityProject/HydrophobicityExplorer",
     author="Sander Cohen-Janes",
-    author_email="scohenjanes@brandeis.edu",
+    author_email="sander.cohen-janes@yale.edu",
     description="Facilitates solubility calculations on a wide range of polymers.",
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
@@ -17,7 +17,7 @@ setuptools.setup(
         'Programming Language :: Python :: 3.6'
     ],
     entry_points={
-        'console_scripts': [ #this section allows us to use functions from the command line. Arguments are ok because main() uses argparse
+        'console_scripts': [ #this section allows us to use functions from the command line.
             # 'command = package.module:function',
             'makePol = mhp.MakePolymer:main',
             'customPol = mhp.custom_input_to_mol_file:main',
