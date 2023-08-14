3cdd67091e12b372d23bed2d173282f0a26fbba8
scohenjanes5 <101665992+scohenjanes5@users.noreply.github.com>
2023-08-13 22:58:31 -0400

    Update testcolor.py

--- testing/testcolor.py
+++ testing/testcolor.py
@@ -1,6 +1,5 @@
 from tqdm import trange, tqdm
 from rich.progress import track
-
 import time
 
 # # Define your function or loop
@@ -12,21 +11,23 @@ import time
 # # Call your function
 # some_function()
 
-# for i in track(range(20), description="Processing..."):
-#     print(i)
-#     time.sleep(1)  # Simulate work being done
+lst = range(20)
+
+for i in track(lst, description="Processing...", disable=True):
+    print(lst[i])
+    time.sleep(.1)  # Simulate work being done
 
 
-from rich.progress import Progress
+# from rich.progress import Progress
 
-with Progress() as progress:
+# with Progress() as progress:
 
-    task1 = progress.add_task("[red]Downloading...", total=1000)
-    task2 = progress.add_task("[green]Processing...", total=1000)
-    task3 = progress.add_task("[cyan]Cooking...", total=1000)
+#     task1 = progress.add_task("[red]Downloading...", total=1000)
+#     task2 = progress.add_task("[green]Processing...", total=1000)
+#     task3 = progress.add_task("[cyan]Cooking...", total=1000)
 
-    while not progress.finished:
-        progress.update(task1, advance=0.5)
-        progress.update(task2, advance=0.3)
-        progress.update(task3, advance=0.9)
-        time.sleep(0.02)
+#     while not progress.finished:
+#         progress.update(task1, advance=0.5)
+#         progress.update(task2, advance=0.3)
+#         progress.update(task3, advance=0.9)
+#         time.sleep(0.02)
