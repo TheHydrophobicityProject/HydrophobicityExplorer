4ede5a68c38fb1c5a8224383597f1abbb8b4cbbd
scohenjanes5 <101665992+scohenjanes5@users.noreply.github.com>
2023-08-15 19:42:07 -0400

    Use a test class to reuse smiles and n

--- testing/test_makepolymer.py
+++ testing/test_makepolymer.py
@@ -23,76 +23,71 @@ from rdkit import Chem
 
 ### Well Well Well, look at how the use of global variables came back to bite me!
 
-def test_reading_from_files():
+class TestPolymerIO:
     n = 3
     smiles = "O" + n * "Cc1ccccc1O" + "CC(C(=O)OC)C"
 
-    pol = Chem.MolFromSmiles(smiles)
+    def test_reading_from_files(self):
+        pol = Chem.MolFromSmiles(self.smiles)
 
-    name="tmp.mol"
-    Chem.MolToMolFile(pol, name)
+        name="tmp.mol"
+        Chem.MolToMolFile(pol, name)
 
-    POL = MakePolymer.read_pol(name, n)
-    os.remove(name)
+        POL = MakePolymer.read_pol(name, self.n)
+        os.remove(name)
 
-    canon_read = Chem.CanonSmiles(POL.smiles)
-    canon_original = Chem.CanonSmiles(smiles)
+        canon_read = Chem.CanonSmiles(POL.smiles)
+        canon_original = Chem.CanonSmiles(self.smiles)
 
-    
-    assert canon_original == canon_read
+        
+        assert canon_original == canon_read
 
-def test_draw_single_pol():
-    smiles = "O" + "Cc1ccccc1O" + "CC(C(=O)OC)C"
-    pol = Chem.MolFromSmiles(smiles)
-    name = "tmp.png"
-    MakePolymer.drawPol(pol, drawName=name)
-    pol2 = Chem.MolFromPNGFile(name)
-    os.remove(name)
+    def test_draw_single_pol(self):
+        pol = Chem.MolFromSmiles(self.smiles)
+        name = "tmp.png"
+        MakePolymer.drawPol(pol, drawName=name)
+        pol2 = Chem.MolFromPNGFile(name)
+        os.remove(name)
 
-    inch = Chem.MolToInchi(pol)
-    inch2 = Chem.MolToInchi(pol2)
+        inch = Chem.MolToInchi(pol)
+        inch2 = Chem.MolToInchi(pol2)
 
-    assert inch == inch2
+        assert inch == inch2
 
-def test_draw_grid_of_pols():
-    smiles = "O" + "Cc1ccccc1O" + "CC(C(=O)OC)C"
+    def test_draw_grid_of_pols(self):
+        POL = MakePolymer.Polymer(self.n, self.smiles)
+        pol = [POL]
 
-    POL = MakePolymer.Polymer(1, smiles)
-    pol = [POL]
-
-    name = "tmp.png"
-    MakePolymer.drawPol(pol, drawName=name)
-    
-    # We can't read from a grid image directly, so just check if file exists
-    assert os.path.exists(name)
-    os.remove(name)
+        name = "tmp.png"
+        MakePolymer.drawPol(pol, drawName=name)
+        
+        # We can't read from a grid image directly, so just check if file exists
+        assert os.path.exists(name)
+        os.remove(name)
 
 
-def test_write_pol_to_file():
-    n = 3
-    smiles = "O" + n * "Cc1ccccc1O" + "CC(C(=O)OC)C"
+    def test_write_pol_to_file(self):
+        POL = MakePolymer.Polymer(self.n, self.smiles)
 
-    POL = MakePolymer.Polymer(n, smiles)
+        nConfs = 5
+        pol_list = []
+        for _ in range(nConfs):
+            pol_h = MakePolymer.optPol(POL.flat)
+            pol_list.append(pol_h)
 
-    nConfs = 5
-    pol_list = []
-    for _ in range(nConfs):
-        pol_h = MakePolymer.optPol(POL.flat)
-        pol_list.append(pol_h)
+        write_name = "tmp.mol"
+        MakePolymer.write_pol(write_name, pol_list)
 
-    write_name = "tmp.mol"
-    MakePolymer.write_pol(write_name, pol_list)
+        read_mol = Chem.MolFromMolFile(write_name)
+        Chem.RemoveStereochemistry(read_mol) # FLAT won't have any
 
-    read_mol = Chem.MolFromMolFile(write_name)
-    Chem.RemoveStereochemistry(read_mol) # FLAT won't have any
+        os.remove(write_name)
 
-    os.remove(write_name)
+        smi1 = Chem.MolToSmiles(POL.flat)
+        smi2 = Chem.MolToSmiles(read_mol)
 
-    smi1 = Chem.MolToSmiles(POL.flat)
-    smi2 = Chem.MolToSmiles(read_mol)
+        smi1 = Chem.CanonSmiles(smi1)
+        smi2 = Chem.CanonSmiles(smi2)
 
-    smi1 = Chem.CanonSmiles(smi1)
-    smi2 = Chem.CanonSmiles(smi2)
-
-    assert smi1 == smi2
+        assert smi1 == smi2
 
